b7d5f91f7dc11d07b94aa783337c53c342be5ec7
LuciaVarano <lucia.varano19@gmail.com>
2025-11-26 10:43:02 -0300

    AgregacionMutantDetectorTest

--- src/test/java/com/example/mutantesGlobal/testServicio/MutantDetectorTest.java
+++ src/test/java/com/example/mutantesGlobal/testServicio/MutantDetectorTest.java
@@ -96,4 +96,160 @@ public class MutantDetectorTest {
         };
         assertTrue(MutantDetector.isMutant(dna));
     }
+
+    @Test
+    public void testMutantWithDiagonalInverseSequence() {
+        String[] dna = {
+                "ATGCGA",
+                "CAGTAC",
+                "TTATAT",
+                "AGAAGG",
+                "CCCCTA",
+                "TCACTG"
+        };
+        assertTrue(MutantDetector.isMutant(dna));
+    }
+
+    @Test
+    public void testNonMutantWithOnlyThreeConsecutive() {
+        String[] dna = {
+                "AAATGA",
+                "CAGTGC",
+                "TTATTT",
+                "AGACGG",
+                "GCGTCA",
+                "TCACTG"
+        };
+        assertFalse(MutantDetector.isMutant(dna));
+    }
+
+    @Test
+    public void testMutantWithExactlyTwoSequences() {
+        String[] dna = {
+                "AAAAGA",
+                "CAGTGC",
+                "TTTTGT",
+                "AGAAGG",
+                "CCTCTA",
+                "TCACTG"
+        };
+        assertTrue(MutantDetector.isMutant(dna));
+    }
+
+    @Test
+    public void testNonMutantAlternatingPattern() {
+        String[] dna = {
+                "ATATATAT",
+                "TATATATA",
+                "ATATATAT",
+                "TATATATA",
+                "ATATATAT",
+                "TATATATA",
+                "ATATATAT",
+                "TATATATA"
+        };
+        assertFalse(MutantDetector.isMutant(dna));
+    }
+
+    @Test
+    public void testMutantWithVerticalAndHorizontal() {
+        String[] dna = {
+                "AAAATG",
+                "TGCAGT",
+                "GCTTCC",
+                "CCCCTC",
+                "GTAGTC",
+                "AGTCAC"
+        };
+        assertTrue(MutantDetector.isMutant(dna));
+    }
+
+    @Test
+    public void testMutantMinimumSize4x4() {
+        String[] dna = {
+                "AAAA",
+                "TGCA",
+                "GCTA",
+                "TTTT"
+        };
+        assertTrue(MutantDetector.isMutant(dna));
+    }
+
+    @Test
+    public void testNonMutantAllDifferent() {
+        String[] dna = {
+                "ATGC",
+                "CGTA",
+                "TACG",
+                "GCAT"
+        };
+        assertFalse(MutantDetector.isMutant(dna));
+    }
+
+    @Test
+    public void testMutantWithDiagonalsOnly() {
+        String[] dna = {
+                "ATGCGA",
+                "CAGTAC",
+                "TTATGT",
+                "AGAAGG",
+                "CCTCTA",
+                "TCACTG"
+        };
+        assertTrue(MutantDetector.isMutant(dna));
+    }
+
+    @Test
+    public void testMutantLargeMatrix10x10() {
+        String[] dna = {
+                "ATGCGATTTT",
+                "CAGTGCAAAT",
+                "TTATGTAAAG",
+                "AGAAGGCCCC",
+                "CCCCTATGCA",
+                "TCACTGAAAA",
+                "ATGCGATCGA",
+                "CAGTGCATGC",
+                "TTATGTTACG",
+                "AGAAGGTCGA"
+        };
+        assertTrue(MutantDetector.isMutant(dna));
+    }
+
+    @Test
+    public void testNonMutantNearMiss() {
+        String[] dna = {
+                "ATGCGA",
+                "CAGTGC",
+                "TTATTT",
+                "AGACGG",
+                "CCGTCA",
+                "TCACTG"
+        };
+        assertFalse(MutantDetector.isMutant(dna));
+    }
+
+    @Test
+    public void testMutantWithOverlappingSequences() {
+        String[] dna = {
+                "AAAAAA",
+                "CAGTGC",
+                "TTATGT",
+                "AGAAGG",
+                "CCCCCC",
+                "TCACTG"
+        };
+        assertTrue(MutantDetector.isMutant(dna));
+    }
+
+    @Test
+    public void testMutantWithAllSameBase() {
+        String[] dna = {
+                "AAAA",
+                "AAAA",
+                "AAAA",
+                "AAAA"
+        };
+        assertTrue(MutantDetector.isMutant(dna));
+    }
 }
